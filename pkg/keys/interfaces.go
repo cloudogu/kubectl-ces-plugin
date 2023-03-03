3c9e1436f9444de70edea5724d52bbf485733d32
Philipp Pixel <boris.schaa@cloudogu.com>
2023-03-03 13:36:31 +0100

    #1 simplify interface comment
    Co-authored-by: Jeremias Weber <jeremias.weber@cloudogu.com>

--- pkg/keys/interfaces.go
+++ pkg/keys/interfaces.go
@@ -13,28 +13,6 @@ type keyProvider interface {
 // manage dogus, their configuration and their states.
 type cesRegistry interface {
 	// Registry is embedded because using our own interfaces easily lets us generate mocks.
-	// Previously this interface made use of our own configurationContext interface like so:
-	// ```
-	// type cesRegistry interface {
-	//	// GlobalConfig returns a configurationContext for the global context
-	//	GlobalConfig() configurationContext
-	//	// HostConfig returns a configurationContext for the host context
-	//	HostConfig(hostService string) configurationContext
-	//	// DoguConfig returns a configurationContext for the given dogu
-	//	DoguConfig(dogu string) configurationContext
-	//	// State returns the state object for the given dogu
-	//	State(dogu string) registry.State
-	//	// DoguRegistry returns an object which is able to manage dogus
-	//	DoguRegistry() registry.DoguRegistry
-	//	// BlueprintRegistry to maintain a blueprint history
-	//	BlueprintRegistry() configurationContext
-	//	// RootConfig returns a WatchConfigurationContext for the root context
-	//	RootConfig() registry.WatchConfigurationContext
-	//	// GetNode returns all keys that are included in any path, packed as Node
-	//	GetNode() (registry.Node, error)
-	// }
-	// ```
-	// Sadly, Go is too stoopid to do recursive duck-typing.
 	// Therefore, we shifted to this pragmatic approach, which is much easier to read and understand anyway.
 	registry.Registry
 }
